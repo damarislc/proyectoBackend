0398af463ed762c81300cb0713b1deb91fd54846
damarislc <damarislc9@gmail.com>
2024-02-24 14:28:25 -0500

    Integrando practica integradora con proyecto backend

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,134 +1,46 @@
 import express from "express";
-import path from "path";
-import __dirname from "../utils.js";
-import CartManager from "../models/cartManager.js";
-import ProductManager from "../models/productManager.js";
+import CartManager from "../dao/cartManager.js";
 
-//Inicializando el router
 const router = express.Router();
+//crea una instancia del CartManager
+const cartManager = new CartManager();
 
-//Path de la "base de datos" (json) de productos
-const cartsdb = path.join(__dirname, "/db", "carts.json");
-const productsdb = path.join(__dirname, "/db", "products.json");
-const cartManager = new CartManager(cartsdb);
-const productManager = new ProductManager(productsdb);
-
-//****Endpoints****
-/**
- * Post para crear el carrito vacío
- */
-router.post("/api/carts", (req, res) => {
-  //Obtiene la promesa del addCart
-  const cartPromise = cartManager.addCart();
-  /** Promesa,
-   * si se cumple, devuelve el carrito creado, el cual contiene
-   * el id del carrito y el arreglo de productos vacío.
-   * Si no se cumple, manda un mensaje de error.
-   */
-  cartPromise
-    .then((cart) => res.json({ "Cart:": cart }))
-    .catch((error) =>
-      res.send({
-        status: "error",
-        error: `Error al agregar el carrito ${error}`,
-      })
-    );
+//Crear carrito
+router.post("/", (req, res) => {
+  //Llama el método addCart para crear el carrito
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
+  cartManager
+    .addCart()
+    .then((cart) => res.send({ status: "success", payload: cart }))
+    .catch((error) => res.send({ status: "error", error: error }));
 });
 
-/**
- * Get para obtener los productos del carrito especificado.
- * Se hace una función anónima asíncrona para poder manipular las promesas.
- * Recibe como parámetro el id del carrito.
- * Modo de ejecución: http://localhost:8080/api/carts/:cid
- */
-router.get("/api/carts/:cid", async (req, res) => {
-  //Obtiene el id desde el parámetro del request
-  const cid = parseInt(req.params.cid);
-  //Si el id no es un número, manda un mensaje de error
-  if (isNaN(cid))
-    return res.send({
-      status: "error",
-      error: "El id tiene que ser un numero",
-    });
-  try {
-    //Obtiene el carrito a partir de su id
-    const cart = await cartManager.getCart(cid);
-    //Obtiene el arreglo de productos.
-    const products = cart.products;
-    //Nuevo arreglo que contendra los productos con sus titulos
-    const productsWithTitle = [];
-    //for para recorrer el arreglo de productos.
-    for (let i = 0; i < products.length; i++) {
-      //Obtiene el producto en el índice correspondiente
-      const product = await productManager.getProductById(products[i].id);
-      //Crea un objeto con el ide del producto, titulo y cantidad
-      const productO = {
-        id: products[i].id,
-        title: product.title,
-        quantity: products[i].quantity,
-      };
-      //lo guarda en el arrglo
-      productsWithTitle.push(productO);
-    }
-    /* Manda el JSON de respuesta de los productos en el carrito 
-       con el titulo correspondiente. */
-    res.json({ "Products in cart:": productsWithTitle });
-  } catch (error) {
-    //Manda un mensaje de error si hubo un error al obtener el carrito o los productos.
-    console.error("Error al obtener el carrito.", error);
-    res.send({
-      status: "error",
-      error: `Error al obtener el carrito. ${error}`,
-    });
-  }
+//Obtener productos del carrito
+router.get("/:cid", (req, res) => {
+  //obtiene el id del carrito desde el params
+  const cid = req.params.cid;
+  //Llamda el método getCart para obtener los productos del carrito correspondiente
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
+  cartManager
+    .getCart(cid)
+    .then((result) => res.send({ status: "sucess", payload: result }))
+    .catch((error) => res.send({ status: "error", error: error }));
 });
 
-/**
- * Post para añadir un producto a un carrito.
- * Se hace una función anónima asíncrona para poder manipular las promesas.
- * Recibe como parámetros el id del carrito y el id del producto.
- * Modo de ejecución: http://localhost:8080/api/carts/:cid/product/:pid
- */
-router.post("/api/carts/:cid/product/:pid", async (req, res) => {
-  //Obtiene el id del carrito
-  const cid = parseInt(req.params.cid);
-  //Obtiene el id del producto
-  const pid = parseInt(req.params.pid);
-
-  //Si el cid o el pid no son número, manda mensaje de error.
-  if (isNaN(cid) || isNaN(pid))
-    return res.send({
-      status: "error",
-      error: "Los ids tienen que ser un numero",
-    });
-
-  let product;
-
-  try {
-    //Revisa si existe el producto a añadir.
-    product = await productManager.getProductById(pid);
-  } catch (error) {
-    //Si hubo un error al obtener el producto, manda mensaje con el error.
-    console.error("Error al obtener el producto.", error);
-    return res.send({
-      status: "error",
-      error: `Error al obtener el producto. ${error}`,
-    });
-  }
-
-  try {
-    //Añade el producto al carrito especificado
-    const cart = await cartManager.addProductToCart(cid, pid);
-    //Manda el JSON con el carrito actualizado.
-    res.json({ Added: cart });
-  } catch (error) {
-    //Si hubo un error al añadir el producto, manda mensaje con el error.
-    console.log(error);
-    res.send({
-      status: "error",
-      error: `Error al añadir el producto al carrito. ${error}`,
-    });
-  }
+//Añadir producto al carrito
+router.post("/:cid/product/:pid", (req, res) => {
+  //Obtiene el id del carrito y del producto desde el params
+  const cid = req.params.cid;
+  const pid = req.params.pid;
+  //Llama el método addProductCart para añadir el producto al carrito
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
+  cartManager
+    .addProductToCart(cid, pid)
+    .then((result) => res.send({ status: "succes", payload: result }))
+    .catch((error) => res.send({ status: "error", error: error }));
 });
 
 export default router;
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,170 +1,113 @@
 import express from "express";
-import path from "path";
-import __dirname from "../utils.js";
-import ProductManager from "../models/productManager.js";
+import ProductManager from "../dao/productManager.js";
 
-//Inicializando el router
 const router = express.Router();
+//crea una instancia del ProductManager
+const productManager = new ProductManager();
 
-//Path de la "base de datos" (json) de productos
-const productsdb = path.join(__dirname, "/db", "products.json");
-const productManager = new ProductManager(productsdb);
-
-//****Endpoints*****
-/**
- * Get que devuelve la lista de todos los productos del archivo si no se
- * incluye un limte. Modo de ejecucion: http://localhost:8080/products
- * Si incluye un limite por medio de query, devuelve solo los productos
- * requeridos. Modo de ejecucion: http://localhost:8080/products?limit=1
- */
-router.get("/api/products", (req, res) => {
-  //Obtiene el limite desde el query request
-  const limit = parseInt(req.query.limit);
-  //Obtiene la promesa con los productos
-  const productsPromise = productManager.getProducts();
-
-  /** Promesa,
-   * si se cumple entonces devuelve todos los los productos o los productos limitados
-   * si no se cumple, manda un mensaje de error con el error obtenido
-   */
-  productsPromise
-    .then((products) => {
-      /**
-       * Si limit no es un numero o
-       * es igual o menor a 0 o
-       * el limite otorgado es mayor al arreglo de productos,
-       * devuelve todos los productos.
-       */
-      if (isNaN(limit) || limit <= 0 || limit > products.length)
-        return res.json(products);
-
-      /**
-       * Si no entro al if anterior quiere decir que contiene un
-       * limite correcto, por lo que crea un nuevo arreglo desde
-       * la posicion 0 hasta el limite de productos solicitados
-       * y los regresa en formato de json.
-       */
-      let productsLimited = products.slice(0, limit);
-      res.json(productsLimited);
-    })
+//Obtener todos los productos
+router.get("/", (req, res) => {
+  //Llama el método getProducts
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
+  productManager
+    .getProducts()
+    .then((products) => res.send({ result: "sucess", payload: products }))
     .catch((error) =>
       res.send({
         status: "error",
-        error: `Error al obtener los productos, ${error}`,
+        error: "Error al obtener los productos, " + error,
       })
     );
 });
 
-/**
- * Get que devuelve el producto con el id solicitado.
- * Recibe como parámetro el id del producto
- * Modo de ejecucion: http://localhost:8080/products/:pid
- */
-router.get("/api/products/:pid", (req, res) => {
-  //Obtiene el id desde el parametro del request
-  const pid = parseInt(req.params.pid);
-  //Si el id no es un numero, manda un msj de error
-  if (isNaN(pid))
-    return res.json({ message: "Error, el id debe ser un numero" });
-  //Obtiene la promesa del producto segun el id del parametro
-  const productPromise = productManager.getProductById(pid);
-  /** Promesa,
-   * si la promesa se cumple entonces devuelve el producto
-   * si no, manda un mensaje de error.
-   */
-  productPromise
+//Obtener producto por id
+router.get("/:pid", (req, res) => {
+  //Obtiene el id del producto desde el params
+  const pid = req.params.pid;
+  //Llama el método getProductById para obtener el producto según su id
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
+  productManager
+    .getProductById(pid)
     .then((product) => {
-      res.json(product);
+      if (result) {
+        res.send({ result: "sucess", payload: product });
+      }
+      res.send({
+        result: "error",
+        error: `El producto con el id ${pid} no existe.`,
+      });
     })
     .catch((error) =>
       res.send({
         status: "error",
-        error: `Error al obtener el producto. ${error}`,
+        error: "Error al obtener el producto, " + error,
       })
     );
 });
 
-/**
- * Post para añadir un nuevo producto.
- * Modo de ejecución: http://localhost:8080/api/products
- * Body raw JSON ejemplo: 
-   {
-    "title": "Rodillo para pintar",
-    "description": "Rodillo de pared, 30 cm",
-    "code": "AASD1456",
-    "price": 75,
-    "status": true,
-    "stock": 10,
-    "category": "Herramientas para pintar",
-    "thumbnail": ["url1", "url2"]
-   }
-   Todos los campos son requeridos excepto thumnail y status,
-   status si no es incluido por default se le asignará true.
- */
-router.post("/api/products", (req, res) => {
-  //Obtiene el body con el JSON del objeto que se agregará
+//Añadir producto
+router.post("/", (req, res) => {
+  //Obtiene el json del product desde el body
   const product = req.body;
-  /** Manda a llamar la promesa para añadir un producto
-   * si se cumple, regresa el producto con un mensaje de éxito,
-   * si no, manda un mensaje de error.
-   */
+  //Llama el método addProduct para añadir el producto a la colección
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
   productManager
     .addProduct(product)
-    .then((p) => res.json({ Success: p }))
-    .catch((err) => res.send({ status: "error", error: err }));
+    .then((result) => res.send({ result: "sucess", payload: result }))
+    .catch((error) =>
+      res.send({
+        status: "error",
+        error: "Error al añadir el producto, " + error,
+      })
+    );
 });
 
-/**
- * Put para actualizar un producto.
- * Modo de ejecución: http://localhost:8080/api/products/:pid
- * no se requiere mandar todos los campos, solo los que se van a actualizar.
- * Si se manda el codigo y no es el mismo y se está repitiendo el código de otro producto,
- * mandará mensaje de error y no se actualizará.
- */
-router.put("/api/products/:pid", (req, res) => {
-  //Obtiene el body con el JSON de los campos que se actualizarán
+//Actualizar producto
+router.put("/:pid", (req, res) => {
+  //Obtiene el id de prodcuto a actualizar y el json de los campos actualizados
+  const pid = req.params.pid;
   const product = req.body;
-  //Obtiene el id del producto que de actualizará
-  const pid = parseInt(req.params.pid);
-  //Si el id no es un número, manda msj de error.
-  if (isNaN(pid))
-    return res.send({
-      status: "error",
-      error: "El id tiene que ser un numero",
-    });
-  /** Manda a llamar la promesa para actualizar el producto,
-   * recibe el id y los nuevos campos en un objeto.
-   * Si se cumple manda un mensaje con el producto actualizado,
-   * si no se cumple manda un mensaje de error.
-   * */
+  //Llama el método updateProduct con el id del producto y los nuevos campos
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
   productManager
     .updateProduct(pid, product)
-    .then((p) => res.json({ Updated: p }))
-    .catch((err) => res.send({ status: "error", error: err }));
+    .then((result) => {
+      if (result) {
+        res.send({ result: "sucess", payload: result });
+      }
+      res.send({
+        result: "error",
+        error: `El producto con el id ${pid} no existe.`,
+      });
+    })
+    .catch((error) =>
+      res.send({
+        status: "error",
+        error: "Error al actualizar el producto, " + error,
+      })
+    );
 });
 
-/**
- * Delete para borrar un producto.
- * Modo de ejecución: http://localhost:8080/api/products/:pid
- */
-router.delete("/api/products/:pid", (req, res) => {
-  //Obtiene el id el producto que se desea eliminar
-  const pid = parseInt(req.params.pid);
-  //Si el id no es un número, manda msj de error.
-  if (isNaN(pid))
-    return res.send({
-      status: "error",
-      error: "El id tiene que ser un numero",
-    });
-  /** Manda a llamar la promesa para borrar el producto,
-   * recibe el id del producto.
-   * Si se cumple manda un mensaje con el producto eliminado,
-   * si no se cumple manda un mensaje de error.
-   * */
+//Eliminar producto
+router.delete("/:pid", async (req, res) => {
+  //Obtiene el id del producto desde el params
+  const pid = req.params.pid;
+  //Llama el método deleteProduct con el id del producto a eliminar
+  //si la promesa es exitosa manda el resultado
+  //sino manda un mensaje de error
   productManager
     .deleteProduct(pid)
-    .then((p) => res.json({ Deleted: p }))
-    .catch((err) => res.send({ status: "error", error: err }));
+    .then((result) => res.send({ result: "sucess", payload: result }))
+    .catch((error) =>
+      res.send({
+        status: "error",
+        error: "Error al eliminar el producto, " + error,
+      })
+    );
 });
 
 export default router;
